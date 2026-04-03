4d052ebaae806f1f64efd65528b867c99082ad66
Rhys <rhys.al.evans@proton.me>
2026-04-03 18:09:09 +0200

    WIP - tarted loading basic SDF

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use std::fs;
+use std::path::{Path, PathBuf};
+
+fn main() -> Result<()> {
+    println!("Welcome to Clipper");
+
+    let sdf = fs::read_to_string("./data/tiny_test_set.sdf")?;
+
+    let mols: Vec<Vec<&str>> = sdf
+        .trim()
+        .split("$$$$")
+        .map(|x| x.trim().lines().collect())
+        .collect();
+
+    println!("{:?}", mols);
+    println!("{:?}", mols.len());
+
+    Ok(())
 }
